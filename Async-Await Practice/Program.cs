de8699931f8308bc752bc606dc33a59fa04c0748
swcc cwcw <nickorici@yahoo.com>
2019-04-16 19:47:54 +0300

    amamammamama

--- Async-Await Practice/Program.cs
+++ Async-Await Practice/Program.cs
@@ -4,48 +4,7 @@ using System.Threading.Tasks;
 using System.Text;
 namespace Async_Await_Practice
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            GetString();
-            Console.Read();
-        }
-
-        static async Task GetString()
-        {
-            await GetAsync();
-        }
-        fefefefefef
-        static async Task<string> GetAsync()
-        {
-            Task t = Task.Run(() => Iterate());
-            Console.WriteLine("started task async");
-            DoExtraWork();
-            await t;
-            Console.WriteLine("Exited await...");
-        }
-
-        private static void DoExtraWork()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                Thread.Sleep(150);
-                Console.WriteLine($"Extra work {i}");
-            }
-        }
-
-        public static int Iterate()
-        {
-            for (int i = 0; i < 250; i++)
-            {
-                if (i % 10 == 0)
-                {
-                    Thread.Sleep(200);
-                    Console.WriteLine($"Reached {i}");
-                }
-            }
-            return 99999;
+   
         }
     }
 }
